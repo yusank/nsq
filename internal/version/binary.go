6b077bbd5c6efffd4f4c405481ed6d37ddbc9567
yusank <yusankurban@gmail.com>
2021-01-26 20:40:59 +0800

    fix: update version

--- internal/version/binary.go
+++ internal/version/binary.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-const Binary = "1.2.2-alpha"
+const Binary = "1.2.2"
 
 func String(app string) string {
 	return fmt.Sprintf("%s v%s (built w/%s)", app, Binary, runtime.Version())
